27fd8e2c5d57383d00079b17d042ccae079ee998
yousef-alaa-mostafa <yousefalaa67@gmail.com>
2022-12-02 21:56:10 +0200

    update

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -80,7 +80,7 @@ export default function Login() {
               press sign up for Regestration
               <br />
               <span className="line">
-                <Link to="/signup">Sign Upp</Link>
+                <Link to="/signup">Sign Up</Link>
               </span>
             </p>
           </section>
