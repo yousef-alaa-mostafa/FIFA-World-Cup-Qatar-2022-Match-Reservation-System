7247592bda47a9f1b5492e5a5cfdc9c1fbc68114
yousef-alaa-mostafa <yousefalaa67@gmail.com>
2022-12-05 01:51:48 +0200

    update

--- src/Components/SiteAdministrator/AllUsers.jsx
+++ src/Components/SiteAdministrator/AllUsers.jsx
@@ -4,13 +4,50 @@ import React from "react";
 import Navbar from "./Navbar";
 
 //used styles
-import style_ from "./AllUsers.module.css";
+import style_ from "./Admin.module.css";
 
 export default function AllUsers() {
+  let user = {
+    name_: "yousef alaa",
+    userName: "yousef_332",
+    email: "yousef@gmail.com",
+    BirthDate: "10/9/2000",
+    Gender: "male",
+    Nationality: "Egypt",
+  };
+  let arr = [1, 1, 1, 1, , 1, 1, 1, 1, 1, 1, 1, 1, , 1, 1, 1, , 1, 1, 1];
   return (
     <div className={style_.mainContainer}>
       <Navbar />
-      All users
+      <table style={{ width: "100%" }}>
+        <tr>
+          <th>Name</th>
+          <th>Username</th>
+          <th>Email</th>
+          <th>BirthDate</th>
+          <th>Gender</th>
+          <th>Nationality</th>
+          <th>Delete user</th>
+        </tr>
+
+        {arr.map((value, index) => {
+          return (
+            <tr key={index}>
+              <td>{user.name_}</td>
+              <td>{user.userName}</td>
+              <td>{user.email}</td>
+              <td>{user.BirthDate}</td>
+              <td>{user.Gender}</td>
+              <td>{user.Nationality}</td>
+              <td style={{ textAlign: "center", verticalAlign: "middle" }}>
+                <button type="button" class="btn btn-danger">
+                  Delete
+                </button>
+              </td>
+            </tr>
+          );
+        })}
+      </table>
     </div>
   );
 }
